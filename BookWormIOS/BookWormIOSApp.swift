b36eb4c35b81397402caf75173e6594ca010611b
Ravid Krisi <ravidkrisi22@gmail.com>
2025-06-09 21:40:41 +0300

    Initial Commit

---
+++ BookWormIOS/BookWormIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormIOSApp.swift
+//  BookWormIOS
+//
+//  Created by Ravid Krisi on 09/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookWormIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookWormIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookWormIOS
+//
+//  Created by Ravid Krisi on 09/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
